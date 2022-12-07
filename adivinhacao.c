edb4cff9c7640cb7d308a311a4492982ae1c9f22
Gustavo Alexandre <gustavoslyth1@gmail.com>
2022-12-07 03:15:53 -0300

    Adicionado sistema de switch

--- adivinhacao.c
+++ adivinhacao.c
@@ -19,21 +19,44 @@ int main() {
 	int tentativas = 1;
 	double pontos = 1000;
 	
-	while(1){
+	int acertou = 0;
+	
+	int nivel;
+	
+	int numerodetentativas;
+	
+	printf("Qual o nível de dificuldade desejada?");
+	printf("[1] - Fácil  | [2] - Médio  | [3] - Difícil\n\n""");
+	printf("Digite o valor:");
+	scanf("%d", &nivel);
+	
+	switch(nivel){
+		case 1:
+		numerodetentativas = 20;
+		break;
+	
+		case 2:
+		numerodetentativas = 15;
+		break;
+	
+		default:
+		numerodetentativas = 10;
+			
+	
+	int i = 1;
+	for(i=1; i <= numerodetentativas; i++){
  	  	printf("\n\nTentativa %d.",tentativas);
  	  	printf("\nQual é o seu chute? ");
  	  	
 	    scanf("\n%d", &chute);
 	    printf("Seu chute foi %d.",chute);
 	    
-  		int acertou = (chute == numerosecreto);		
+  		acertou = (chute == numerosecreto);		
 		int maior = (chute>numerosecreto);	
 		
    		if(acertou){
-		printf("\nParabéns, você acertou!\n");
 		break;
 		
-					
 }
     	if(chute<0){
 		printf("\nVocê não pode chutar um número negativo!");
@@ -51,9 +74,12 @@ int main() {
 		pontos = pontos - abs(pontosperdidos);     //Abs torna todos os números positivos, evitando bugs  na subtração de pontos;
 
 }
+if(acertou) {
 		printf("\nFim de jogo!");
 		printf("\nVocê precisou de %d tentativas para acertar.\n", tentativas);
 		printf("Total de pontos: %.1f", pontos); //O .1 no % é referente a quantidade de casas decimais que serão apresentadas no resultado;
 		return 0;
+		}
+	}
 }
 		
